2bf88f4844027cb8a277042a15b471672936d67c
Kathlia Carlos <lia.carlos23@gmail.com>
2020-09-30 10:39:17 +0800

    add listing model, and in index page

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Listing
+
+admin.site.register(User)
+admin.site.register(Listing)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    product = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    date_created = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=200, blank=True)
+    lister = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.product} is ${self.price}"
