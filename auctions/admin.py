5bf628a729b187f1effd055207a9392d9532fd9d
Kathlia Carlos <lia.carlos23@gmail.com>
2020-09-30 15:09:10 +0800

    migrate Bid

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Listing
+from .models import User, Listing, Bid
 
 admin.site.register(User)
 admin.site.register(Listing)
+admin.site.register(Bid)
--- auctions/models.py
+++ auctions/models.py
@@ -14,4 +14,14 @@ class Listing(models.Model):
     lister = models.CharField(max_length=64)
 
     def __str__(self):
-        return f"{self.product} is ${self.price}"
+        return f"{self.product} : ${self.price}"
+
+
+class Bid(models.Model):
+    bid = models.DecimalField(max_digits=12, decimal_places=2)
+    bidder = models.ForeignKey(User, related_name="bids", on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Listing, on_delete=models.CASCADE,)
+
+    def __str__(self):
+        return f"{self.bidder} bids for {self.bid} on {self.product}"
