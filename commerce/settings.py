19c80a1021aea67c1839c092c6299b3273c46b51
Kathlia Carlos <lia.carlos23@gmail.com>
2020-10-03 09:46:02 +0800

    modify settings for image upload

--- commerce/settings.py
+++ commerce/settings.py
@@ -120,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# For uploading images
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# For Static files
+STATICFILES_DIR = os.path.join(
+    os.path.join(BASE_DIR, 'static')
+)
